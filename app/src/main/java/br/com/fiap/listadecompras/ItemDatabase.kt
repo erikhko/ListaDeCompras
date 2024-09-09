ce2445e2b557772a35789df49d3d8efcaed6df71
Erik <erikhko@hotmail.com>
2024-09-09 12:58:53 -0300

    Correção da classe ItemDatabase

--- app/src/main/java/br/com/fiap/listadecompras/ItemDatabase.kt
+++ app/src/main/java/br/com/fiap/listadecompras/ItemDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 @Database(entities = [ItemModel::class], version = 1)
-abstract class ItemDatabase : void RoomDatabase() {
+abstract class ItemDatabase : RoomDatabase() {
 
     abstract fun itemDao(): ItemDao
 }
